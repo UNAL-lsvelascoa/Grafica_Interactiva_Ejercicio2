b1750b56cf23472799c40fd8d99300a82bd18fd9
Ld Erick Stevens ES. Velasco Amaya <e.velasco@intergroup.com>
2017-05-22 17:18:07 -0500

    shots

---
+++ js/actions.js
@@ -0,0 +1,113 @@
+﻿function collect1(player, collectable) {
+    collectable.kill();
+    score1 += 1;
+    scoreText1.text = 'Tiros: ' + score1;
+}
+function collect2(player, collectable) {
+    collectable.kill();
+    score2 += 1;
+    scoreText2.text = 'Tiros: ' + score2;
+}
+function launchCollectable() {
+    if (game.rnd.integerInRange(1, 50) == 1) {
+        var star = collectables.create(game.rnd.integerInRange(0, 800), 0, 'collectable');
+
+        star.body.gravity.y = GRAVITY;
+        star.body.velocity.x = game.rnd.integerInRange(-500, 500);
+        star.body.collideWorldBounds = true;
+
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+        star.body.bounce.x = 1;
+    }
+}
+function changeTime() {
+    if (fps % 60 == 0) {
+        time--;
+        timeText.text = 'Tiempo: ' + time;
+        if (time == 0) {
+            finishGame();
+        }
+    }
+}
+function move(player, left, right, up, indexPlayer) {
+    if (left.isDown) {
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+        directions[indexPlayer] = LEFT_DIRECTION;
+    }
+    else {
+        if (right.isDown) {
+            player.body.velocity.x = 150;
+            player.animations.play('right');
+            directions[indexPlayer] = RIGHT_DIRECTION;
+        }
+        else {
+            player.animations.stop();
+            switch (directions[indexPlayer]) {
+                case LEFT_DIRECTION:
+                    player.frame = 2;
+                    break;
+                case RIGHT_DIRECTION:
+                    player.frame = 5;
+                    break;
+            }
+        }
+    }
+    if (up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -450;
+    }
+    if (player.body.touching.down) {
+        player.body.velocity.x = 0;
+    }
+}
+function shoot1() {
+    var shooting = shots1.create(player1.body.x + 12, player1.body.y + 12, 'shoot1');
+    shooting.scale.setTo(0.4, 0.4);
+    switch (directions[0]) {
+        case LEFT_DIRECTION:
+            shooting.body.velocity.x = -SHOOT_VELOCITY;
+            break;
+        case RIGHT_DIRECTION:
+            shooting.body.velocity.x = SHOOT_VELOCITY;
+            break;
+    }
+    score1--;
+    scoreText1.text = 'Tiros: ' + score1;
+}
+function shoot2() {
+    var shooting = shots2.create(player2.body.x+12, player2.body.y+12, 'shoot2');
+    shooting.scale.setTo(0.4, 0.4);
+    switch (directions[1]) {
+        case LEFT_DIRECTION:
+            shooting.body.velocity.x = -SHOOT_VELOCITY;
+            break;
+        case RIGHT_DIRECTION:
+            shooting.body.velocity.x = SHOOT_VELOCITY;
+            break;
+    }
+    score2--;
+    scoreText2.text = 'Tiros: ' + score2;
+}
+
+function hitTo1(player, shoot) {
+    shoot.kill();
+    lifeBar1.scale.x -= game.rnd.realInRange(0.01, 0.05);
+    if (lifeBar1.scale.x < 0.15) {
+        lifeBar1.lineStyle(5, RED, 1);
+    } else {
+        if (lifeBar1.scale.x < 0.4) {
+            lifeBar1.lineStyle(5, YELLOW, 1);
+        }
+    }
+}
+function hitTo2(player, shoot) {
+    shoot.kill();
+    lifeBar2.scale.x -= game.rnd.realInRange(0.01, 0.05);
+    if (lifeBar2.scale.x < 0.15) {
+        lifeBar2.lineStyle(5, RED, 1);
+    } else {
+        if (lifeBar2.scale.x < 0.4) {
+            lifeBar2.lineStyle(5, YELLOW, 1);
+        }
+    }
+}
---
+++ js/initElements.js
@@ -0,0 +1,44 @@
+﻿function initLifeBar(lifeBar) {
+    lifeBar.lineStyle(5, GREEN, 1);
+    lifeBar.moveTo(0, 0);
+    lifeBar.lineTo(250, 0);
+    lifeBar.scale.x = 1;
+}
+
+function initPlayer(player) {
+    game.physics.arcade.enable(player);
+    player.body.gravity.y = 0;
+    player.body.collideWorldBounds = true;
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    player.frame = 4;
+}
+
+function initKeys() {
+    up1 = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    down1 = game.input.keyboard.addKey(Phaser.Keyboard.S);
+    left1 = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    right1 = game.input.keyboard.addKey(Phaser.Keyboard.D);
+    up2 = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    down2 = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    left2 = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    right2 = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    enter = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    p = game.input.keyboard.addKey(Phaser.Keyboard.P);
+    u = game.input.keyboard.addKey(Phaser.Keyboard.U);
+    g = game.input.keyboard.addKey(Phaser.Keyboard.G);
+    k = game.input.keyboard.addKey(Phaser.Keyboard.K);
+}
+
+function initPlatforms(platforms) {
+    platforms.enableBody = true;
+    var ground = platforms.create(0, game.world.height - 32, 'ground');
+    ground.scale.setTo(2, 1);
+    ground.body.immovable = true;
+
+    for (var i = 0; i < 7; i++) {
+        var cloud = platforms.create(game.rnd.integerInRange(-100, 700), game.rnd.integerInRange(100, 450), 'cloud');
+        cloud.body.immovable = true;
+        cloud.scale.setTo(0.4, 0.4);
+    }
+}
